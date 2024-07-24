31141ecf9c73e441223b572c871d0bbd46bf4408
you@example.com <you@example.com>
2024-07-25 04:21:01 +0900

    add 60km keyboard

---
+++ keyboards/60km/config.h
@@ -0,0 +1,14 @@
+// キーマトリクスの大きさ
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 7
+
+// キーのピン設定
+// UNUSED_PINSには何も書かなくてOK
+// #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
+// #define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2 }
+#define UNUSED_PINS
+
+// ダイオードの向き
+/* COL2ROW, ROW2COL */
+#define DIODE_DIRECTION COL2ROW
