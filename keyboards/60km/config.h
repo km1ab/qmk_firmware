8a40fe5091d655e49d82113ded408ffbd53b0021
KM <km.app.developer@gmail.com>
2024-07-25 05:28:17 +0900

    update json for via update keymap as via apply layer pos led, update keymap.

--- keyboards/60km/config.h
+++ keyboards/60km/config.h
@@ -1,14 +1,4 @@
-// キーマトリクスの大きさ
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 7
+#pragma once
 
-// キーのピン設定
-// UNUSED_PINSには何も書かなくてOK
-// #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
-// #define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2 }
-#define UNUSED_PINS
+#define SPLIT_HAND_PIN B5
 
-// ダイオードの向き
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
