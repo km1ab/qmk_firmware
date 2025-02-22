513f7154a98a9cfe103b628cef82f86729523cf1
KM <you@example.com>
2025-02-22 18:01:43 +0900

    fix LED

--- keyboards/60km/keymaps/default/config.h
+++ keyboards/60km/keymaps/default/config.h
@@ -1,7 +1,3 @@
-#pragma once
-
-#define DYNAMIC_KEYMAP_LAYER_COUNT 4
-
 #define RGBLIGHT_LAYERS
 
 #define RGBLIGHT_EFFECT_BREATHING
@@ -16,8 +12,4 @@
 #define RGBLIGHT_EFFECT_TWINKLE
 #define RGBLIGHT_LED_COUNT 20     // Number of LEDs in your strip
 #define RGBLED_SPLIT {10,10}
-// However, there may be a change in qmk specifications
-// ----------------------- *  *  *  *  *
-// ----------------index-- 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19
-// -----------LED number--20,19,10, 9, 8, 1, 2, 3, 4, 5, 6, 7,11,12,13,14,15,16,17,18
-#define RGBLIGHT_LED_MAP {19,18, 9, 8, 7, 0, 1, 2, 3, 4, 5, 6,10,11,12,13,14,15,16,17 }
+#define RGBLIGHT_LED_MAP { 5,6,7,8,9,10,11,0,1,2,12,13,14,15,16,17,18,19,3,4 }
