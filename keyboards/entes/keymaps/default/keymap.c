1a5f1e79eeea4601b8d5006d08286691bca5f81f
KM <km.app.developer@gmail.com>
2025-03-22 15:26:08 +0900

    apply encoder-setting to json.

--- keyboards/entes/keymaps/default/keymap.c
+++ keyboards/entes/keymaps/default/keymap.c
@@ -3,22 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-/*
-    "encoder": {
-        "rotary": [
-                { "pin_a": "GP1", "pin_b": "GP2", "resolution": 2 },
-                { "pin_a": "GP4", "pin_b": "GP3", "resolution": 2 }
-        ]
-    },
-    
-    "encoder": {
-        "rotary": [
-                { "pin_a": "GP26", "pin_b": "GP27", "resolution": 2 },
-                { "pin_a": "GP1", "pin_b": "GP2", "resolution": 2 }
-        ]
-    },
-
-*/
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
@@ -33,10 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] =   { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  }
-    // [0] =   { ENCODER_CCW_CW(KC_PGUP, KC_PGDN)  }
-    //                  Encoder 1                                     Encoder 2
 };
-
 #endif
 
 // bool encoder_update_user(uint8_t index, bool clockwise) {
